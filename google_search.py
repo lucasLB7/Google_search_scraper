1cef14fb804cd093f7154b72f8158b75a46c6b87
Lucas Lambert <plucaslambert@gmail.com>
2018-11-14 14:56:34 +0300

    further changes with code, working on multiple files.

--- google_search.py
+++ google_search.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests 
 from urllib.request import urlopen
+import psycopg2
 try: 
     from googlesearch import search 
 except ImportError:  
@@ -11,13 +12,16 @@ print("Enter Keywords or search phrase")
 query = input()
 print("Enter the number of pages you want to go through")
 page_num = int(input())
-  
-for j in search(query, tld="co.in", num=page_num, stop=1, pause=2):
-    search = j
-    page = urlopen(search)
-    soup = BeautifulSoup(page, 'html.parser')
-    with open('results.html', "w") as file:
-        file.write(str(soup))
+# conn = psycopg2.connect("dbname='google_crawl' user='postgres' password='postgres'")
+try:
+    for j in search(query, tld="co.in", num=page_num, stop=1, pause=2):
+        search = j
+        page = urlopen(search)
+        soup = BeautifulSoup(page, 'html.parser')
+        with open('results.html', "w") as file:
+            file.write(str(soup))
+except:
+    pass
     
     
     
