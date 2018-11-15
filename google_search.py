ef290cc1dd21d32ad46bb012cdf77984349134c1
Lucas Lambert <plucaslambert@gmail.com>
2018-11-15 15:16:27 +0300

    working on the postgresql database data. The full contents of "soup" should go into the table. So far my issue is that I am unable to insert the "soup" variable into the table... :/

--- google_search.py
+++ google_search.py
@@ -1,42 +1,74 @@
 from bs4 import BeautifulSoup
-import requests 
+import requests
 from urllib.request import urlopen
 import psycopg2
-try: 
-    from googlesearch import search 
-except ImportError:  
-    print("No module named 'google' found") 
-  
+import sys
+try:
+    from googlesearch import search
+except ImportError:
+    print("No module named 'google' found")
+
+
+
+
+
 # to search
-print("Enter Keywords or search phrase") 
+print("Enter Keywords or search phrase")
 query = input()
+mylist = []
 print("Enter the number of pages you want to go through")
 page_num = int(input())
-# conn = psycopg2.connect("dbname='google_crawl' user='postgres' password='postgres'")
+
 try:
     for j in search(query, tld="co.in", num=page_num, stop=1, pause=2):
         search = j
         page = urlopen(search)
         soup = BeautifulSoup(page, 'html.parser')
-        with open('results.html', "w") as file:
-            file.write(str(soup))
+        con = None
+
+        try:
+            con = psycopg2.connect("host='localhost' dbname='google_crawl' user='crawler' password='crawl'")
+            cur = con.cursor()
+            cur.execute("CREATE TABLE Products(Id INTEGER PRIMARY KEY, website VARCHAR(20), html_code VARCHAR)")
+            cur.execute("INSERT INTO Products (Id, website, html_code) VALUES(1,"test",soup)"),
+            con.commit()
+        except psycopg2.DatabaseError as e:
+            if con:
+                con.rollback()
+
+            print('Error %s' % e)
+            sys.exit(1)
+
+        finally:
+            if con:
+                con.close()
+
+
+        # with open('results.html', "w") as file:
+        #     file.write(str(soup))
+
+
+
 except:
-    pass
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    print("error")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # url = input(j)
     # r = requests.get("http://"+url)
     # data = r.text
     # soup = BeautifulSoup(data)
 
     # for link in soup.find_all('a'):
-    #     print(link.get('href')) 
-
+    #     print(link.get('href'))
