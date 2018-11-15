4635256e1fb6fde33ff76da421f57607448f150a
Lucas Lambert <plucaslambert@gmail.com>
2018-11-15 16:40:13 +0300

    Converted to raw litterals as database can't handle html string. Added a counter for the unique id of the table. Still getting an error...... nkt

--- google_search.py
+++ google_search.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from urllib.request import urlopen
+from psycopg2.extensions import adapt, register_adapter, AsIs
 import psycopg2
 import sys
 try:
@@ -16,26 +17,33 @@ except ImportError:
 print("Enter Keywords or search phrase")
 query = input()
 mylist = []
+con = None
 print("Enter the number of pages you want to go through")
 page_num = int(input())
-
+id_index = 0
+con = psycopg2.connect("host='localhost' dbname='google_crawl' user='crawler' password='crawl'")
+cur = con.cursor()
+cur.execute("CREATE TABLE Products(Id INTEGER PRIMARY KEY, website VARCHAR(20), html_code VARCHAR)")
 try:
     for j in search(query, tld="co.in", num=page_num, stop=1, pause=2):
         search = j
         page = urlopen(search)
         soup = BeautifulSoup(page, 'html.parser')
-        con = None
+        raw_soup = ("r'%s'"%(soup,))
+        id_index += 1
+
+        # def adapt_soup(soup):
+        #     return AsIs("'(%s)'" % (soup))
+        # register_adapter(soup, adapt_soup)
+
 
         try:
-            con = psycopg2.connect("host='localhost' dbname='google_crawl' user='crawler' password='crawl'")
-            cur = con.cursor()
-            cur.execute("CREATE TABLE Products(Id INTEGER PRIMARY KEY, website VARCHAR(20), html_code VARCHAR)")
-            cur.execute("INSERT INTO Products VALUES(1,'test',soup)"),
+
+            cur.execute("INSERT INTO Products VALUES(%s, %s, %s)",[id_index, "test",raw_soup]),
             con.commit()
         except psycopg2.DatabaseError as e:
             if con:
                 con.rollback()
-
             print('Error %s' % e)
             sys.exit(1)
 
